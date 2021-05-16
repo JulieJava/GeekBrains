0e48994d98e13f17e4bb4614c3bc8027b88cdec6
JulieJava <study.account@mail.ru>
2021-05-16 16:56:36 +0300

    Домашняя работа к уроку №2 в курсе "Java Core для тестировщиков"

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] array = new String[4][];
+        String[] first = new String[]{"2", "2", "3", "76"};
+        String[] second = new String[]{"15", "2", "4", "2"};
+        String[] third = new String[]{"13", "2", "3", "12"};
+        String[] fourth = new String[]{"7", "6", "43", "6"};
+        array[0] = first;
+        array[1] = second;
+        array[2] = third;
+        array[3] = fourth;
+
+        ArrayHandler handler = new ArrayHandler();
+
+        try {
+            int result = handler.handle(array);
+            System.out.println(result);
+        } catch (RuntimeException ex) {
+            System.out.println("Произошла ошибка: " + ex.getMessage());
+        }
+        System.out.println("Конец программы");
+    }
+}
