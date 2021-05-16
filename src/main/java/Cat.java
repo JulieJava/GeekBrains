576227ac59cd200bae9f1a57c0838f784c35a8a0
JulieJava <study.account@mail.ru>
2021-05-16 15:03:23 +0300

    Домашняя работа к уроку №1 в курсе "Java Core для тестировщиков"

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat implements CanRun, CanJump{
+    private int maxLength;
+    private int maxHeight;
+
+    public Cat(int maxLength, int maxHeight) {
+        this.maxLength = maxLength;
+        this.maxHeight = maxHeight;
+    }
+
+    @Override
+    public int run () {
+        System.out.println("Кот бежит");
+        return maxLength;
+    }
+    @Override
+    public int jump () {
+        System.out.println("Кот прыгает");
+        return maxHeight;
+    }
+}
